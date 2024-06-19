26de145b1a6ade5e5ad5350177f63a40ec113d13
Rommel <rommel@coreform.co>
2024-06-19 22:54:51 +0800

    implement logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,66 @@
+import { ChangeEvent, useEffect, useState } from "react";
 import Button from "./components/ui/Button";
 import Input from "./components/ui/Input";
 
 const pages = ["Page 1", "Page 2", "Page 3", "Page 4"];
 
 const App = () => {
+  const [checkAll, setCheckAll] = useState<boolean>(false);
+  const [checkedPages, setCheckedPages] = useState<string[]>([]);
+  const [done, setDone] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (checkedPages.length === pages.length) {
+      setCheckAll(true);
+    } else {
+      setCheckAll(false);
+    }
+  }, [checkedPages]);
+
+  const handleChangeCheckAll = (e: ChangeEvent<HTMLInputElement>) => {
+    setCheckAll(e.target.checked);
+    if (e.target.checked) {
+      setCheckedPages(pages);
+    } else {
+      setCheckedPages([]);
+    }
+  };
+
+  const handleChangeCheckPage = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.checked) {
+      setCheckedPages((prev) => [...prev, e.target.name]);
+    } else {
+      setCheckedPages((prev) => [...prev.filter((page) => page !== e.target.name)]);
+    }
+  };
+
+  const handleClickDone = () => {
+    if (done) {
+      setCheckedPages([]);
+    } else {
+      setCheckedPages(pages);
+    }
+    setDone((prev) => !prev);
+  };
+
   return (
     <div className="flex justify-center pt-20">
       <div className="font-montserrat p-5 rounded-lg shadow-[0_8px_15px_1px_rgba(20,20,20,0.3)] w-1/2">
-        <Input label="All pages" />
+        <Input page="All pages" checked={checkAll} disabled={done} handleChangeCheckbox={handleChangeCheckAll} />
         <hr className="my-5 h-0.2 bg-slate-300" />
         {pages.map((page) => {
-          return <Input key={page} label={page} />;
+          return (
+            <Input
+              key={page}
+              page={page}
+              checked={checkedPages.includes(page)}
+              disabled={done}
+              handleChangeCheckbox={handleChangeCheckPage}
+            />
+          );
         })}
         <hr className="my-5 h-0.2 bg-slate-300" />
-        <Button label="Done" onClick={() => {}} />
+        <Button label={done ? "Reset" : "Done"} onClick={handleClickDone} />
       </div>
     </div>
   );
--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,16 +1,27 @@
-import { FC } from "react";
+import { ChangeEvent, FC } from "react";
 
 interface Props {
-  label: string;
+  page: string;
+  checked: boolean;
+  disabled: boolean;
+  handleChangeCheckbox: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
-const Input: FC<Props> = ({ label }) => {
+const Input: FC<Props> = ({ page, checked, disabled, handleChangeCheckbox }) => {
   return (
     <div className="flex justify-between m-2 items-center">
-      <label htmlFor={label} className="ms-2 text-lg font-normal text-black">
-        {label}
+      <label htmlFor={page} className="ms-2 text-lg font-normal text-black">
+        {page}
       </label>
-      <input id={label} type="checkbox" name={label} className="w-4 h-4 text-yellow hover:bg-blue-light" />
+      <input
+        checked={checked}
+        onChange={(e) => handleChangeCheckbox(e)}
+        disabled={disabled}
+        id={page}
+        type="checkbox"
+        name={page}
+        className="w-4 h-4 text-yellow hover:bg-blue-light"
+      />
     </div>
   );
 };
