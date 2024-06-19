746172086a3f525623a9d1aa98c46ca7067138d5
Rommel <rommel@coreform.co>
2024-06-19 22:34:22 +0800

    implement box with shadow

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 const App = () => {
-  return <>Test</>;
+  return (
+    <div className="flex justify-center pt-20">
+      <div className="font-montserrat p-5 rounded-lg shadow-[0_8px_15px_1px_rgba(20,20,20,0.3)] w-1/2">Test</div>
+    </div>
+  );
 };
 
 export default App;
