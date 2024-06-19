03b2eb65a0d934073a32f87d6eeffea93117972b
Rommel <rommel@coreform.co>
2024-06-19 22:39:10 +0800

    add button and checkbox on main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import Button from "./components/ui/Button";
+import Input from "./components/ui/Input";
+
 const App = () => {
   return (
     <div className="flex justify-center pt-20">
       <div className="font-montserrat p-5 rounded-lg shadow-[0_8px_15px_1px_rgba(20,20,20,0.3)] w-1/2">
-        <button onClick={() => {}} className="bg-yellow hover:bg-yellow-light h-10 w-full text-lg rounded-md">
-          Done
-        </button>
+        <Input label="All pages" />
+        <hr className="my-5 h-0.2 bg-slate-300" />
+        <Button label="Done" onClick={() => {}} />
       </div>
     </div>
   );
