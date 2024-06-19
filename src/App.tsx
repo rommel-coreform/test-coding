ddbb845260e3963b2ccfd2e9aa8aa13ebe31d17f
Rommel <rommel@coreform.co>
2024-06-19 22:35:40 +0800

    add button on main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 const App = () => {
   return (
     <div className="flex justify-center pt-20">
-      <div className="font-montserrat p-5 rounded-lg shadow-[0_8px_15px_1px_rgba(20,20,20,0.3)] w-1/2">Test</div>
+      <div className="font-montserrat p-5 rounded-lg shadow-[0_8px_15px_1px_rgba(20,20,20,0.3)] w-1/2">
+        <button onClick={() => {}} className="bg-yellow hover:bg-yellow-light h-10 w-full text-lg rounded-md">
+          Done
+        </button>
+      </div>
     </div>
   );
 };
