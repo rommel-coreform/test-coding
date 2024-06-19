d2ac6ff4f47dc4599972887b48c7faa1da3cdfa6
Rommel <rommel@coreform.co>
2024-06-19 22:25:25 +0800

    setup theme and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        yellow: "#FFCE22",
+        "yellow-light": "#FFD84D",
+        blue: "#2469F6",
+        "blue-light": "#5087F8",
+        black: "#1F2128",
+      },
+      fontFamily: {
+        montserrat: ["Montserrat"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
