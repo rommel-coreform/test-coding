c6ffa9876819c3378fe90e419148b448de9e6891
Rommel <rommel@coreform.co>
2024-06-19 22:40:40 +0800

    add pages checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import Button from "./components/ui/Button";
 import Input from "./components/ui/Input";
 
+const pages = ["Page 1", "Page 2", "Page 3", "Page 4"];
+
 const App = () => {
   return (
     <div className="flex justify-center pt-20">
       <div className="font-montserrat p-5 rounded-lg shadow-[0_8px_15px_1px_rgba(20,20,20,0.3)] w-1/2">
         <Input label="All pages" />
+        <hr className="my-5 h-0.2 bg-slate-300" />
+        {pages.map((page) => {
+          return <Input key={page} label={page} />;
+        })}
         <hr className="my-5 h-0.2 bg-slate-300" />
         <Button label="Done" onClick={() => {}} />
       </div>
